a91a0f0e8f9f326c3326d9df7a2a3636bad8a538
Pedro Salgado <peterdaemonx@gmail.com>
2017-09-24 17:33:49 +0200

    Modify app.module.ts

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,10 +2,14 @@ import { NgModule }      from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent }  from './app.component';
+import  { HeaderComponent } from './Common/header.component';
+import  { FootComponent } from './Common/footer.component';
+import  { LeftComponentNavigation } from './Common/left.componen.navigation';
+import  { RigthComponentContent } from './Common/rigth.component.content';
 
 @NgModule({
   imports:      [ BrowserModule ],
-  declarations: [ AppComponent ],
+  declarations: [ AppComponent, HeaderComponent, FootComponent, LeftComponentNavigation, RigthComponentContent ],
   bootstrap:    [ AppComponent ]
 })
 export class AppModule { }
